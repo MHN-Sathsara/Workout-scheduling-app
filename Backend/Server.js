757fdac94ce8f907c8047ce5c164510659a2bf79
Sathsara Hewage <sathsarahewage@gmail.com>
2024-05-22 22:36:59 +0530

    node is working

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,6 +3,11 @@ const express = require('express')
 //expressapp
 const app = express()
 
+//routs
+app.get('/', (req, res) =>{
+    res.json({})
+})
+
 //listen for req
 app.listen(4000, () => {
     console.log('Listening on port 4000 its working !')
