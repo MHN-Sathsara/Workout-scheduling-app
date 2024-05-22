de33e08548ee2a9d355506a3c3f2dee227304f58
Sathsara Hewage <sathsarahewage@gmail.com>
2024-05-22 22:24:21 +0530

    redone

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,14 +1,9 @@
 const express = require('express')
 
-
 //expressapp
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
-
 //listen for req
 app.listen(4000, () => {
-    console.log('Listeneing on port 4000 its working')
+    console.log('Listening on port 4000 its working !')
 })
