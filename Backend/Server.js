73f7cd005f5df4d3872a4eadc9ca8462eda883b4
Sathsara Hewage <86362354+MHN-Sathsara@users.noreply.github.com>
2024-07-13 01:34:37 +0530

    mdb Connections
    issues - no connection

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //expressapp
@@ -17,8 +18,17 @@ app.use((req, res, next) => {
 //routs
 app.use('/api/workouts',workoutRoutes)
 
-//listen for req
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port',process.env.PORT)
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  //listen for req
+  app.listen(process.env.PORT, () => {
+  console.log('COnnected to db & Listening on port',process.env.PORT)
 })
+})
+.catch((error) => {
+  console.log(error)
+})
+
+
 
