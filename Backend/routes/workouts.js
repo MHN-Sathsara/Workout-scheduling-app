608a763bd963406fb98e431d893b618abdcc0485
Sathsara Hewage <86362354+MHN-Sathsara@users.noreply.github.com>
2024-07-13 11:40:13 +0530

    Schemas

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const router = express.Router()
-
+const workout = require('../models/workoutmodel.js')
 //GEt all Workouts
 router.get('/', (req, res) => {
     res.json({mssg: 'GETT ALL WORKOUTS!'})
@@ -12,8 +11,14 @@ router.get('/:id',(req, res) =>{
 })
 
 //POST a new workout
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new workout!'})
+router.post('/', async(req, res) => {
+    const {title, load, reps} = req.body
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
 })
 //Delete a workout
 router.delete('/:id', (req, res) => {
