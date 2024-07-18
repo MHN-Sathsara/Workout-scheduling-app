f7ad96e5071358d0a31cc96be1da7b0d93cf6423
Sathsara Hewage <86362354+MHN-Sathsara@users.noreply.github.com>
2024-07-18 13:42:43 +0530

    Frontend started
    Fixed loading error issue with react.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-
-
-//pages and com
-
-import Home from "./pages/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <div className="pages">
-        <Routes>
-          <Route path="/" 
-          element={<Home/>} />
-        </Routes>
-      </div>
-  
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
